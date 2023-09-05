cc7306085eb70c0548211d6a4ddf5dd2fe9bc4d3
Wi-Go <akaza.xeros@gmail.com>
2023-09-05 20:57:48 +0200

    Navigation between login and register, NotFoundPage started

---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+
+const NotFoundPage = () => {
+  return (
+    <div>
+      <h1>NotFoundPage</h1>
+      <p>
+        <Link to="/">Back to Home</Link>
+      </p>
+    </div>
+  );
+};
+export default NotFoundPage;
